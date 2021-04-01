38ed2ae4091bae5e0c5d7fb155bb2bc6f8508d3d
rafael eduardo <rafaelMejia@rafaels-MacBook-Pro-2.local>
2021-04-01 17:17:26 -0600

    initializing repository

---
+++ src/main.js
@@ -0,0 +1,6 @@
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router"; // <-- This imports index.js from the /router directory
+import store from "./store";
+
+createApp(App).use(store).use(router).mount("#app");
---
+++ src/services/EventService.js
@@ -0,0 +1,22 @@
+/* eslint-disable */
+import axios from 'axios'
+
+const apiClient = axios.create({
+  baseURL: 'https://my-json-server.typicode.com/remchh/events',
+  withCredentials: false,
+  headers: {
+    Accept: 'application/json',
+    'Content-Type': 'application/json'
+  }
+})
+
+export default {
+  getEvents() {
+    return apiClient.get('/events')
+  },
+    //Added new call
+    getEvent(id) {
+      return apiClient.get('/events/' + id)
+    }
+}
+
